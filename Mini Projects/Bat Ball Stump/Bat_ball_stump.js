54b94a7be759d6ded6e2e9ad7d00aa1875154cdc
rana41-hub <pratapranak41@gmail.com>
2026-06-13 13:10:26 +0530

    Improved Game Logic

--- Mini Projects/Bat Ball Stump/Bat_ball_stump.js
+++ Mini Projects/Bat Ball Stump/Bat_ball_stump.js
@@ -1,9 +1,19 @@
+let score = {
+  won : 0,
+  lost : 0,
+  tie : 0,
+};
+
+let computer_choice;
+let computer_choiceMsg
+let user_choice;
+let result_msg;
+
 function computer_player (){
 
   let random = Math.random() * 3;                             
-  let computer_choice;
-  let result_msg;
-  let computer_choiceMsg;
+
+
   if(random > 0 && random <= 1) {
      computer_choice = 'Bat';
   }
@@ -19,50 +29,68 @@ function computer_player (){
 
 function alert_message () {
 
-  alert(`Your choice is Bat , ${computer_choiceMsg} , ${result_msg}`);
+  alert(`Your choice is ${user_choice} , ${computer_choiceMsg} , ${result_msg} , 
+    
+    Score is 
+    
+    won : ${score.won}
+    lose : ${score.lost}
+    tie : ${score.tie}`);
 }
 
 function user_bat() {
 
    if(computer_choice === 'Bat') {
     result_msg = `It's a Tie`;
+    score.tie++;
     
   }
   else if (computer_choice === 'Ball') {
     result_msg = `You won`;
+    score.won++;
   }
   else {
     result_msg = `Computer Won`;
+    score.lost++;
   };
   return result_msg;
+  
 };
 
 function user_ball() {
 
   if(computer_choice === 'Bat') {
     result_msg = `Computer Won`;
+    score.lost++;
     
   }
   else if (computer_choice === 'Ball') {
     result_msg = `It's a Tie`;
+    score.tie++;
   }
   else {
     result_msg = `You Won`;
+    score.won++;
   };
   return result_msg;
+  
 };
 
 function user_stump(){
 
     if(computer_choice === 'Bat') {
-    result_msg = `Computer Won`;
+    result_msg = `You Won`;
+    score.won++;
     
   }
   else if (computer_choice === 'Ball') {
-    result_msg = `You Won`;
+    result_msg = `Computer Won`;
+    score.lost++;
   }
   else {
     result_msg = `It's a Tie`;
+    score.tie++;
   };
   return result_msg;
+  
 };
---
+++ Notes/Objects.js
@@ -0,0 +1,25 @@
+//Object is a non-primitve data type in JS . Stores multiple values in the form of Key value pairs .
+
+//Declaration Syntax -->  
+
+// let product = {
+// company : Reliance,
+// price : 882,
+// 'ratings-worldwide' : 4.5,
+// };
+
+//Accessing Values -->
+
+//console.log(product.company);    Looks for exact key name in the object
+
+//console.log(product['ratings-worldwide'])  Can be used if key name contains special characters such as -,$ etc
+
+//let rate = 'ratings-worldwide';
+
+//console.log(product[rate])   Can be used to store the value of key in separate variable then use it 
+
+//Deleting Key value pair in object -->
+
+//delete product.company;
+
+//delete product['ratings-worldwide'];
